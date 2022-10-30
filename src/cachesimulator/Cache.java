d755af80fa87b293419a0cc19cb3cacfabc86df4
Willian Rodrigues <willianrdrigues@hotmail.com>
2022-10-30 17:38:44 -0300

    erros do array corrigido

--- src/cachesimulator/Cache.java
+++ src/cachesimulator/Cache.java
@@ -12,23 +12,26 @@ import java.util.List;
 /*
 cache_simulator 256 4 1 R 1 bin_100.bin
 cache_simulator 128 2 4 R 1 bin_1000.bin
+cache_simulator 512 8 2 R 1 vortex.in.sem.persons.bin
  */
 public class Cache {
 
+    //Declaração de todas as variáveis para o funcionamento da Cache;
     private List<List<String>> cache;
     private String arquivoEntrada;
     private Integer bSize;
     private Integer nSets;
     private Integer assoc;
-    private int missCompulsorio;
-    private int missConflito;
-    private int missCapacidade;
-    private int hit;
-    private int flagSaida;
-    private int totalAcessos;
+    private float missCompulsorio;
+    private float missConflito;
+    private float missCapacidade;
+    private float hit;
+    private int flagSaida; //Flag para saber qual tipo de saída o usuário quer 1 ou 0;
+    private float totalAcessos;
 
     public Cache(Input input) {
-        this.arquivoEntrada = input.getArquivoEntrada();
+        //Cada variável recebe seus respectivos valores de acordo com a entrada
+        this.arquivoEntrada = input.getArquivoEntrada();// arquivo entrada recebe o arquivo .bin
         Integer nLinhas = input.getNsets(); // input.getAssoc();
         this.bSize = input.getBsize();
         this.nSets = input.getNsets();
@@ -36,13 +39,12 @@ public class Cache {
         this.assoc = input.getAssoc();
         this.flagSaida = input.getFlagSaida();
         for (int i = 0; i < input.getAssoc(); i++) {
-            this.cache.add(Arrays.asList(new String[nLinhas]));
+            this.cache.add(Arrays.asList(new String[nLinhas]));//Adiciona os valores da cache em uma matriz;
         }
-
     }
 
     public void codeCache() {
-        lerEndereço();
+        lerEndereço();//Le todos os endereços da Matriz de valores da cache
     }
 
     //pega endereço .bin transforma byte>int>StringBinary
@@ -51,9 +53,9 @@ public class Cache {
             FileInputStream arquivo = new FileInputStream(new File(this.arquivoEntrada));
             DataInputStream arquivoOut = new DataInputStream(arquivo);
             while (true) {
-                String nextLine = Integer.toBinaryString(arquivoOut.readInt());
-                this.totalAcessos++;
-                formataBinario(nextLine);
+                String nextLine = Integer.toBinaryString(arquivoOut.readInt());//Transforma os números binarios em Ints e depois Strings;
+                this.totalAcessos++; //Para cada passada no while significa que ele passou para o próximo número ou seja aumenta +1 no total de acessos;
+                formataBinario(nextLine);//Ativa a função que pega e transforma o binario do arquivo .bin em 32 bits, ou seja adiciona zeros a esquerda até ficar com 32;
 
             }
         } catch (IOException ex) {
@@ -69,65 +71,45 @@ public class Cache {
         separaBinario(binario);
     }
 
-    private void separaBinario(String binario) {
+    private void separaBinario(String binario) {//Faz os calculos para achar o Offset, indice e o tag do endereço específico
         Integer nBitsOffset = log2(bSize);
         Integer nBitsIndice = log2(nSets);
         Integer nBitsTag = 32 - nBitsIndice - nBitsOffset;
         String offset = binario.substring(32 - nBitsOffset);
         String indice = binario.substring(nBitsTag, 32 - nBitsOffset);
         String tag = binario.substring(0, nBitsTag);
-
-        //System.out.println("tag=" + tag);
-        //System.out.println("indice=" + indice);
-        //System.out.println("offset=" + offset);
-        //System.out.println("\n");
-        addCache(indice, tag);
+        addCache(indice, tag);//Função que adiciona o número a cache
     }
 
     private void addCache(String indice, String tag) {
         int endereco = Integer.parseInt(indice, 2) % this.nSets;
         int flag = 0;
-        /*if (this.assoc == 1) {
-            if (cache.get(0).get(endereco) == null) { // verifica se o bit de val = 0
-                cache.get(0).set(endereco, tag);
+        for (int i = 0; i < this.assoc; i++) {
+            if (cache.get(i).get(endereco) == null) { // veririfca se o bit val = 0 ou seja verifica se este local da cache não está vazio
                 this.missCompulsorio++;
-            } else if (cache.get(0).get(endereco).equals(tag)) { // verifica se o tag da cache é igual ao do endereco
+                cache.get(i).set(endereco, tag);//adiciona o número neste local
+                break;
+            } else if (cache.get(i).get(endereco).equals(tag)) {//verficia se o tag é igual, se for é hit
                 this.hit++;
+                break;
             } else { //verifica se é miss conflito ou capacidade
                 if (verificaCache() == false) {
                     this.missConflito++;
+                    flag = 1;
                 } else {
                     this.missCapacidade++;
+                    flag = 1;
                 }
-                cache.get(0).set(endereco, tag);
             }
-        } else {*/
-            for (int i = 0; i < this.assoc; i++) {
-                if (cache.get(i).get(endereco) == null) { // veririfca se o bit val = 0
-                    this.missCompulsorio++;
-                    cache.get(i).set(endereco, tag);
-                } else if (cache.get(i).get(endereco).equals(tag)) {
-                    this.hit++;
-                } else { //verifica se é miss conflito ou capacidade
-                    if (verificaCache() == false) {
-                        this.missConflito++;
-                        flag = 1;
-                    } else {
-                        this.missCapacidade++;
-                        flag = 1;
-                    }
-                }
-
-            //}
             if (flag == 1) {
                 cache.get(random()).set(endereco, tag); //adiciona de forma random o tag na cache
             }
         }
     }
 
-    public boolean verificaCache() {
+    public boolean verificaCache() {    //função que verifica se é miss compusório ou de capacidade, verificando se a cache está vazia ou não
         for (int f = this.assoc - 1; f >= 0; f--) {
-            for (int j = this.nSets; j >= 0; j--) {
+            for (int j = this.nSets - 1; j >= 0; j--) {
                 if (cache.get(f).get(j) == null) {
                     return false; // cache n esta cheia
                 }
@@ -136,26 +118,26 @@ public class Cache {
         return true; // cache cheia
     }
 
-    public void resultado() {
+    public void resultado() {//Printa o resultado de acordo com a flag de saida: 0 ou 1
 
         if (this.flagSaida == 1) {
-            System.out.println("qtd de hit: " + this.hit);
-            System.out.println("qtd de compulsorio: " + this.missCompulsorio);
-            System.out.println("qtd de conflito: " + this.missConflito);
-            System.out.println("qtd de capacidade: " + this.missCapacidade);
+            float missesTotal = this.missCapacidade + this.missCompulsorio + this.missConflito;
+            System.out.println(this.totalAcessos + " " + this.hit / this.totalAcessos + " " + missesTotal / this.totalAcessos + " " + this.missCompulsorio / missesTotal + " " + this.missCapacidade / missesTotal + " " + this.missConflito / missesTotal);
+        } else {
+            System.out.println("Taxa de hits: " + this.hit / this.totalAcessos);
         }
 
     }
 
     //metodo para calcular log2 //
-    private int log2(Integer number) {
+    private int log2(Integer number) {//Faz um log de base 2 para qualquer valor
 
         int resultado = (int) (Math.log(number) / Math.log(2));
         return resultado;
     }
 
     //gera um numero aleatorio //
-    private int random() {
+    private int random() {//Randomiza um valor para qualquer faixa de valores
         return (int) ((Math.random() * (this.assoc - 0)) + 0);
     }
 
--- src/cachesimulator/Input.java
+++ src/cachesimulator/Input.java
@@ -16,9 +16,9 @@ public class Input {
 
 // metodo que separa subString dentro da String de entrada //    
     private void splitCode(String inputCode) {
-        String[] arraySplit = inputCode.split(" ");
+        String[] arraySplit = inputCode.split(" ");//Arraya de Vetores para a entrada de valores Ex: cache_simulator 256 4 1 R 1 bin_100.bin
 
-        if (arraySplit.length == 7) {
+        if (arraySplit.length == 7) { //Pega cada valor de acordo com usa posição no vetor String de entrada
             this.nsets = Integer.parseInt(arraySplit[1]);
             this.bsize = Integer.parseInt(arraySplit[2]);
             this.assoc = Integer.parseInt(arraySplit[3]);
